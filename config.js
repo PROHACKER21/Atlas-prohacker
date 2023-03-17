9eff080f2401191f9fd110223251116796f3d031
PROHACKER21 <101635131+PROHACKER21@users.noreply.github.com>
2023-03-17 13:01:01 +0530

    Update config.js

--- config.js
+++ config.js
@@ -32,20 +32,20 @@
 require("dotenv").config();
 let gg = process.env.MODS;
 if (!gg) {
-  gg = "918101187835";   // You can Change this number //
+  gg = "917249362297,918092300145";   // You can Change this number //
 }
 
 // -------------------------------------------------------------- //
 
 
 global.owner = gg.split(",");
-global.mongodb = process.env.MONGODB || "mongodb+srv://fantox:xrto71r@cluster0.dnlowts.mongodb.net/?retryWrites=true&w=majority";
+global.mongodb = process.env.MONGODB || "mongodb+srv://aimhacker2144:SANNY2144@cluster0.gkg4gpb.mongodb.net/?retryWrites=true&w=majority";
 global.sessionId = process.env.SESSION_ID || "ok";
-global.prefa = process.env.PREFIX || "-";
+global.prefa = process.env.PREFIX || ".";
 global.tenorApiKey =
   process.env.TENOR_API_KEY || "AIzaSyCyouca1_KKy4W_MG1xsPzuku5oa8W358c";
-global.packname = process.env.PACKNAME || `Atlas MD`;
-global.author = process.env.AUTHOR || "by: Team Atlas";
+global.packname = process.env.PACKNAME || `ANONYMOUS`;
+global.author = process.env.AUTHOR || "by:SANNY";
 global.port = process.env.PORT || "8000";
 
 module.exports = {
